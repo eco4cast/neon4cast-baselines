528ca8b05bcce442d4159d0d68c0765b46e31453
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-08-22 11:55:11 -0400

    if statement included to account for sites with no target data for one variable resulting in targets_use being empty. Returns an empty df

--- R/fablePersistenceModelFunction.R
+++ R/fablePersistenceModelFunction.R
@@ -34,29 +34,41 @@ RW_forecast <- function(site, var, h,
     # rather than today
     dplyr::filter(time < forecast_starts$start_date)
   
-  if (transformation == 'log') {
-    RW_model <- targets_use %>%
-      fabletools::model(RW = fable::RW(log(observed)))
+  if (nrow(targets_use) == 0) {
+    message('no targets available, no forecast run')
+    empty_df <- data.frame('variable' = character(),
+                            'site_id' = character(),
+                            '.model' = character(),
+                             'time' = lubridate::ymd(),
+                            '.rep' = character(),
+                            '.sim' = numeric())
+    
+    return(empty_df)
+    
+  } else {
+    if (transformation == 'log') {
+      RW_model <- targets_use %>%
+        fabletools::model(RW = fable::RW(log(observed)))
+    }
+    if (transformation == 'log1p') {
+      RW_model <- targets_use %>%
+        fabletools::model(RW = fable::RW(log1p(observed)))
+    }
+    if (transformation == 'none') {
+      RW_model <- targets_use %>%
+        fabletools::model(RW = fable::RW(observed))
+    }
+    
+    if (bootstrap == T) {
+      forecast <- RW_model %>% fabletools::generate(
+          h = as.numeric(forecast_starts$h),
+          bootstrap = T,
+          times = boot_number
+        )
+    }  else
+      forecast <- RW_model %>% fabletools::forecast(h = as.numeric(forecast_starts$h))
+    message('forecast finished')
+    return(forecast)
   }
-  if (transformation == 'log1p') {
-    RW_model <- targets_use %>%
-      fabletools::model(RW = fable::RW(log1p(observed)))
-  }
-  if (transformation == 'none') {
-    RW_model <- targets_use %>%
-      fabletools::model(RW = fable::RW(observed))
-  }
-  
-  if (bootstrap == T) {
-    forecast <-
-      RW_model %>% fabletools::generate(
-        h = as.numeric(forecast_starts$h),
-        bootstrap = T,
-        times = boot_number
-      )
-  }  else
-    forecast <-
-    RW_model %>% fabletools::forecast(h = as.numeric(forecast_starts$h))
-  return(forecast)
   
 }
