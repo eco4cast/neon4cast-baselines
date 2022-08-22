803cfd153c88489741dc31c8dff9371c304924af
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-08-22 11:56:20 -0400

    add RW persistence to aquatics null models

--- run_aquatics_baselines.R
+++ run_aquatics_baselines.R
@@ -5,4 +5,5 @@ print(paste0("Running Creating Aquatics baselines at ", Sys.time()))
 
 print(paste0("Running daily climatology at ", Sys.time()))
 source("models/aquatics_climatology.R")
+source("models/aquatics_persistenceRW.R")
 print(paste0("Completed daily climatology ", Sys.time()))
