3a219b5c9ceaf13dc098a32a7d756305dc7c0a5a
rqthomas <rqthomas@vt.edu>
2022-08-22 13:38:38 +0000

    fix file name

--- run_phenology_baselines.R
+++ run_phenology_baselines.R
@@ -9,4 +9,4 @@ print(paste0("Completed daily climatology ", Sys.time()))
 
 print(paste0("Running daily persistence at ", Sys.time()))
 source("models/phenology_persistence.R")
-print(paste0("Completed daily persistance ", Sys.time()))
+print(paste0("Completed daily persistence ", Sys.time()))
