22ccc995078fe2d432d3649bb2e70a0d9f97ab89
QuinnThomas <rqthomas@vt.edu>
2022-08-30 09:16:16 -0400

    Update run_phenology_baselines.R

--- run_phenology_baselines.R
+++ run_phenology_baselines.R
@@ -10,3 +10,5 @@ print(paste0("Completed daily climatology ", Sys.time()))
 print(paste0("Running daily persistence at ", Sys.time()))
 source("models/phenology_persistenceRW.R")
 print(paste0("Completed daily persistenceRW ", Sys.time()))
+
+RCurl::url.exists("https://hc-ping.com/a5a12c66-6e38-4415-aa40-4eb24881a949")
