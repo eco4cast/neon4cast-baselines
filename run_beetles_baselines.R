c9c6320fc125af679b303a65acff160639298e56
QuinnThomas <rqthomas@vt.edu>
2022-08-30 09:17:16 -0400

    Update run_beetles_baselines.R

--- run_beetles_baselines.R
+++ run_beetles_baselines.R
@@ -6,3 +6,5 @@ print(paste0("Running Creating Beetles baselines at ", Sys.time()))
 print(paste0("Running mean model at ", Sys.time()))
 source("models/beetles_mean.R")
 print(paste0("Completed mean model ", Sys.time()))
+
+RCurl::url.exists("https://hc-ping.com/28df2e97-6036-4fa1-9d8a-6aa9fbaa0726")
