d4172952528b4476f79f0c82e77e8cad70a11492
QuinnThomas <rqthomas@vt.edu>
2022-08-30 09:15:32 -0400

    Update run_aquatics_baselines.R

--- run_aquatics_baselines.R
+++ run_aquatics_baselines.R
@@ -9,3 +9,5 @@ print(paste0("Completed daily climatology ", Sys.time()))
 print(paste0("Running daily persistence at ", Sys.time()))
 source("models/aquatics_persistenceRW.R")
 print(paste0("Completed daily persistence ", Sys.time()))
+
+RCurl::url.exists("https://hc-ping.com/a848914e-9abf-45e4-bcf3-27f570cc3623")
