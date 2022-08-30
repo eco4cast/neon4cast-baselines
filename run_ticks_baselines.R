1eb4e7b6258021bb7fc06565b391de48fa994535
QuinnThomas <rqthomas@vt.edu>
2022-08-30 09:16:41 -0400

    Update run_ticks_baselines.R

--- run_ticks_baselines.R
+++ run_ticks_baselines.R
@@ -9,3 +9,5 @@ print(paste0("Completed climatology ", Sys.time()))
 print(paste0("Running mean at ", Sys.time()))
 source("models/ticks_mean.R")
 print(paste0("Completed mean ", Sys.time()))
+
+RCurl::url.exists("https://hc-ping.com/9fd0a2f4-13b3-4d11-880b-1487a1d801ca")
